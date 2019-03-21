6de6f86c7f76fc24c72dc96f5c5b7b1aec825862
1codechic <1codechic@devchics.com>
2019-03-21 13:32:40 -0400

    added events controller and route to display events

---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,8 @@
+class Api::EventsController < ApplicationController
+
+  def index
+    response = HTTP.get("https://app.ticketmaster.com/discovery/v2/events.json?apikey=#{ENV["API_KEY"]}")
+
+    render json: response.parse
+  end
+end
